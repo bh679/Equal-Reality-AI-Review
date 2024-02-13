2b5fc781b2d77fc74119c4a9b676cce857a86113
Brennan Hatton <brennan@brennanhatton.com>
2024-02-13 15:35:42 +0800

    Fixing quest build issues

--- Runtime/AIReviewManager.cs
+++ Runtime/AIReviewManager.cs
@@ -68,61 +68,81 @@ namespace EqualReality.ReviewAI
 		// Start is called before the first frame update
 		void Start()
 		{
-			
+			Debug.Log("Start");
 			guidePrompts = new string[guidePromptText.Length];
+			Debug.Log("Load prompts");
 			for(int i = 0 ;i < guidePromptText.Length; i++)
 			{
+				Debug.Log("Prompt:"+i);
 				StartCoroutine(LoadTextFile(guidePromptText[i], (result, i) => 
 				{
 					Debug.Log(result);
 					guidePrompts[i] = result; 
+					Debug.Log("guidePrompt Loaded: "+ result);
 					loaded++;
 				}, i));
 			}
 			
+			Debug.Log("preQuestion prompts");
+			Debug.Log("preQuestionText: "+ preQuestionText);
 			StartCoroutine(TextFile.Load(preQuestionText, (result) => 
 			{
 				Debug.Log(result);
 				preQuestion = result; 
+				Debug.Log("preQuestion Loaded: "+ result);
 			}));
 			
+			Debug.Log("dataFormatExmple prompts");
+			Debug.Log("dataFormatExmpleText: "+ dataFormatExmpleText);
 			StartCoroutine(TextFile.Load(dataFormatExmpleText, (result) => 
 			{
 				Debug.Log(result);
 				dataFormatExmple = result; 
+				Debug.Log("dataFormatExmple Loaded: "+ result);
 				loaded++;
 			}));
 			
+			Debug.Log("genericCallToAction prompts");
+			Debug.Log("genericCallToActionText: "+ genericCallToActionText);
 			StartCoroutine(TextFile.Load(genericCallToActionText, (result) => 
 			{
 				Debug.Log(result);
 				genericCallToAction = result; 
+				Debug.Log("genericCallToAction Loaded: "+ result);
 				loaded++;
 			}));
 			
+			Debug.Log("acknowledgeCTA prompts");
+			Debug.Log("acknowledgeCTAText: "+ acknowledgeCTAText);
 			StartCoroutine(TextFile.Load(acknowledgeCTAText, (result) => 
 			{
 				Debug.Log(result);
 				acknowledgeCTA = result; 
+				Debug.Log("acknowledgeCTA Loaded: "+ result);
 				loaded++;
 			}));
 			
+			Debug.Log("finalCTA prompts");
+			Debug.Log("finalCTAText: "+ finalCTAText);
 			StartCoroutine(TextFile.Load(finalCTAText, (result) => 
 			{
 				Debug.Log(result);
 				finalCTA = result; 
+				Debug.Log("finalCTA Loaded: "+ result);
 				loaded++;
 			}));
 		}
 		
 		void SetGPT()
 		{
+			Debug.Log("SetGPT");
 			if(gpt != null)
 				DestroyImmediate(gpt.gameObject);
 				
 			gpt = Instantiate(aiPrefab,this.transform);
 			gpt.transform.localPosition = Vector3.zero;
 			
+			Debug.Log("gpt:" + gpt);
 			gpt.prompWrapper.prePrompt2 = new string[3 + guidePromptText.Length];
 			for(int i = 0 ;i < guidePromptText.Length; i++)
 			{
@@ -132,20 +152,27 @@ namespace EqualReality.ReviewAI
 			gpt.prompWrapper.prePrompt2[guidePromptText.Length + 1] = dataFormatExmple;
 			gpt.prompWrapper.prePrompt2[guidePromptText.Length + 2] = genericCallToAction;
 			
+			Debug.Log("gpt prompts wrapped");
 			gpt.onSendGPT.AddListener(SaveSentData);
 			gpt.onRecieveResponse.AddListener(SaveReceivedData);
+			Debug.Log("gpt listerners set");
 			
 			voice = gpt.GetComponent<VoiceRecorder>();
 			
+			Debug.Log("voice:"+voice);
 			
 			elSpeaker = gpt.GetComponent<ELSpeaker>();
+			Debug.Log("elSpeaker:"+elSpeaker);
 			elSpeaker.onFinishPlayingVoice.AddListener(NextInteraction);
+			Debug.Log("elSpeaker listener");
 			
 			onAIReset.Invoke(gpt, voice, elSpeaker);
+			Debug.Log("AI Reset finished");
 		}
 		
 		public void SaveSentData(string sentData)
 		{
+			Debug.Log("SaveSentData");
 			RequestData requestData = new RequestData();
 			requestData.prompt = sentData;
 			requestData.max_tokens = gpt.max_tokens;
@@ -159,6 +186,7 @@ namespace EqualReality.ReviewAI
 		
 		public void SaveReceivedData(string receivedData)
 		{
+			Debug.Log("SaveReceivedData");
 			currentInteraction.SetResponse(receivedData);
 			interactions.Add(currentInteraction);
 			
@@ -166,6 +194,7 @@ namespace EqualReality.ReviewAI
 		
 		public void StartReview(ReviewData _reviewData)
 		{
+			Debug.Log("StartReview");
 			reviewData = _reviewData;
 			
 			SetGPT();
@@ -247,8 +276,8 @@ namespace EqualReality.ReviewAI
 			
 			if(interactions.Count == 0)
 				nextQuestion.Question = reviewData.CurrentQuestion;
-				else
-			nextQuestion.Question = InteractionData.MostRecentInteraction(interactions).generatedText;//InteractionData.MostRecentInteraction(gpt.interactions).generatedText;
+			else
+				nextQuestion.Question = InteractionData.MostRecentInteraction(interactions).generatedText;//InteractionData.MostRecentInteraction(gpt.interactions).generatedText;
 			
 			
 			gpt.prompWrapper.prePrompt2[guidePromptText.Length] = 
@@ -258,13 +287,23 @@ namespace EqualReality.ReviewAI
 			voice.MonitorMic();
 		}
 		
-		public IEnumerator LoadTextFile(string filPath, System.Action<string, int> result, int i)
+		public IEnumerator LoadTextFile(string filePath, System.Action<string, int> result, int i)
 		{
-			string path = "file://" + Application.streamingAssetsPath + filPath;
-			Debug.Log(path);
+			string path;
+			
+#if UNITY_ANDROID && !UNITY_EDITOR
+			// For Android, directly use the path without "file://" prefix
+			path = Application.streamingAssetsPath + filePath;
+#else
+			// For other platforms, prepend "file://" to the path
+			path = "file://" + Application.streamingAssetsPath + filePath;
+#endif
+
+			Debug.Log("path in LoadTextFile: "+path);
 			using (UnityWebRequest www = UnityWebRequest.Get(path))
 			{
 				yield return www.SendWebRequest();
+
 				if (www.result != UnityWebRequest.Result.Success)
 				{
 					Debug.Log(www.error);
@@ -272,11 +311,11 @@ namespace EqualReality.ReviewAI
 				}
 				else
 				{
-					//Debug.Log(www.downloadHandler.text);
 					result(www.downloadHandler.text, i);
 				}
 			}
 		}
+
 		
 		
 
