6a189c40141eb48666676c0280d595f06870ebfb
Brennan Hatton <brennan@brennanhatton.com>
2024-03-20 09:16:14 +0800

    same comment as preivous

---
+++ Runtime/GPT Intergartion/LoudnessShake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EqualReality.ReviewAI.GPTAIIntergration
+{
+	public class LoudnessEvent : MonoBehaviour
+	{
+		public float minimum = 0.9f;
+		
+		public VoiceRecorder vr;
+		
+		public UnityEvent onToLoud, onNowGood;
+		bool isToLoud = false;
+		
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+		    isToLoud = !(vr.loudness > minimum);
+	    }
+	
+	    // Update is called once per frame
+	    void Update()
+		{
+			if((vr.loudness > minimum) != isToLoud)
+		    {
+				if(isToLoud)
+					onNowGood.Invoke();
+				else
+					onToLoud.Invoke();
+		    }
+		    
+		    
+		    isToLoud = (vr.loudness > minimum);
+	    }
+	}
+}
