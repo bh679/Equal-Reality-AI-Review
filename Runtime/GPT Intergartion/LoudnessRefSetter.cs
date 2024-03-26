c94953cf2b4229f428db1a3157acab6aedc1f960
Brennan Hatton <brennan@brennanhatton.com>
2024-03-26 10:40:11 +0800

    same

---
+++ Runtime/GPT Intergartion/LoudnessRefSetter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EqualReality.ReviewAI;
+
+namespace EqualReality.ReviewAI.GPTAIIntergration
+{
+		
+	public class LoudnessRefSetter : MonoBehaviour
+	{
+		public AIReviewUI aIReviewUI;
+		
+		public LoudnessToHeight[] loudnessToHeights;
+		public LoudnessToImageFill[] loudnessToImageFills;
+		public LoudnessEvent[] loudnessEvents;
+		
+		void Reset()
+		{
+			aIReviewUI = GameObject.FindObjectOfType<AIReviewUI>();
+			loudnessToHeights = GameObject.FindObjectsOfType<LoudnessToHeight>();
+			loudnessToImageFills = GameObject.FindObjectsOfType<LoudnessToImageFill>();
+			loudnessEvents = GameObject.FindObjectsOfType<LoudnessEvent>();
+		}
+		
+		
+		void Start()
+		{
+			aIReviewUI.onSetAIRefts.AddListener(SetAIRefs);
+		}
+		
+		void SetAIRefs()
+		{
+			for(int i = 0; i < loudnessToHeights.Length; i++)
+			{
+				loudnessToHeights[i].vr = aIReviewUI.voice;
+			}
+			
+			for(int i = 0; i < loudnessToImageFills.Length; i++)
+			{
+				loudnessToImageFills[i].vr = aIReviewUI.voice;
+			}
+			
+			for(int i = 0; i < loudnessEvents.Length; i++)
+			{
+				loudnessEvents[i].vr = aIReviewUI.voice;
+			}
+			
+		}
+	}
+
+}
